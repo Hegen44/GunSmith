72d7cd39102f3af6edb37f939fee204d72276b41
Hiu-Chun Lo <hilo@crm.ucsc.edu>
2017-05-07 22:34:11 -0700

    add more gun script

--- Assets/Script/Gun Script/AR.cs
+++ Assets/Script/Gun Script/AR.cs
@@ -2,15 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AR : MonoBehaviour {
+public class AR : Gun {
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    private float nextFire = 0.0F;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + this.fireRate;
+            this.shoot(this.transform.position, this.transform.rotation);
+        }
+    }
+
+    void init(string n, int v)
+    {
+        this.Name = n;
+        this.Weight = Random.Range(0, 3);
+        this.tier = v;
+        this.magazinesize = Random.Range(0,3) * v;
+        this.damage = Random.Range(0, 3) * v;
+        this.reloadspeed = Random.Range(0, 3) * v;
+        this.ammo = Random.Range(0, 3) * v;
+        this.spread = Random.Range(0, 3) * v;
+        this.fireRate = Random.Range(0, 3) * v;
+        this.range = Random.Range(0, 3) * v;
+        this.recoil = Random.Range(0, 3) * v;
+    }
+     /*
+
+        //GunName.js
+        var company = ["Johnson ", "Jefferson ", "Thompson ", " Nikosa "];
+        var firstname = ["malicious ", "delicious ", "legendary "];
+        var lastname = ["banana ", "waffle ", " kitten "];
+        var type = ["smg", "lmg", "ar", "pistol"];
+        var wepmanager : GameObject;
+        var gui : GameObject;
+        var gunname : String;
+        var stats : String;
+        var explosion : Transform;
+
+        function Start(){
+        wepmanager = gameObject.Find("weapons");
+        gui = GameObject.Find("weptext");
+        var i = Random.Range (0, company.Length);
+        var j = Random.Range (0, firstname.Length);
+        var k = Random.Range (0, lastname.Length);
+        var l = Random.Range (0, type.Length);
+        var damage = gameObject.GetComponent("ShootScript").damage;
+        var rate = gameObject.GetComponent("ShootScript").delay;
+        var accu = gameObject.GetComponent("ShootScript").shotSpread;
+        var gunnum = wepmanager.transform.childCount;
+        gunname = company[i]+firstname[j]+lastname[k]+type[l];
+        stats = Mathf.Round(rate*10)/10+"-"+Mathf.Round(accu*10)/10+"-"+Mathf.Round(damage);
+
+        transform.name=gunnum+"_"+gunname;
+
+        }
+
+        function FixedUpdate ()
+        {
+        gui.guiText.text=gunname + " ("+stats+")";
+
+        }
+    */
 }
--- Assets/Script/Gun Script/Gun.cs
+++ Assets/Script/Gun Script/Gun.cs
@@ -2,30 +2,102 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Gun: MonoBehaviour
+public abstract class Gun : MonoBehaviour
 {
-    public string Name;
-    public float Weight;
-
-    public int magazinesize;
-    public int damage;
-    public float reloadspeed;
-    public float ammo;
-    public float spread;
-    public float firerate;
-    public int magzine;
-    public float range;
-    public float recoil;
 
     public GameObject bullet;
 
-    public virtual void shoot(vector3 p, Quaternion r)
+    private string _name;
+    private float _weight;
+    private int _tier;
+    private int _magazinesize;
+    private int _damage;
+    private float _reloadspeed;
+    private float _ammo;
+    private float _spread;
+    private float _firerate;
+    private int _ammocount;
+    private float _range;
+    private float _recoil;
+
+
+    #region setter and getter
+
+    public string Name
     {
-        Instantiate(bullet, p, r);
+        get { return _name; }
+        set { _name = value; }
+    }
+    public float Weight
+    {
+        get { return _weight; }
+        set { _weight = value; }
+    }
+
+    public int tier
+    {
+        get { return _tier; }
+        set { _tier = value; }
+    }
+    public int magazinesize
+    {
+        get { return _magazinesize; }
+        set { _magazinesize = value; }
+    }
+    public int damage
+    {
+        get { return _damage; }
+        set { _damage = value; }
+    }
+    public float reloadspeed
+    {
+        get { return _reloadspeed; }
+        set { _reloadspeed = value; }
+    }
+    public float ammo
+    {
+        get { return _ammo; }
+        set { _ammo = value; }
+    }
+    public float spread
+    {
+        get { return _spread; }
+        set { _spread = value; }
+    }
+    public float fireRate
+    {
+        get { return _firerate; }
+        set { _firerate = value; }
+    }
+    public int ammocount
+    {
+        get { return _ammocount; }
+        set { _ammocount = value; }
+    }
+    public float range
+    {
+        get { return _range; }
+        set { _range = value; }
+    }
+    public float recoil
+    {
+        get { return _recoil; }
+        set { _recoil = value; }
+    }
+    #endregion
+
+    public virtual void shoot(Vector3 p, Quaternion r)
+    {
+        Instantiate(this.bullet, p, r);
     }
 
     public virtual void reload()
     {
-        magzine = magazinesize;
+        this.ammocount = this.magazinesize;
+    }
+
+    public virtual void Init()
+    {
+
     }
 }
